02600c0e3b40192e9489b3721179f9a77c3281f7
MarinaPolis <134808992+MarinaPolis@users.noreply.github.com>
2024-10-10 23:42:14 +0300

    Update ілююша багато

--- Program.cs
+++ Program.cs
@@ -1,39 +1,32 @@
-﻿using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Runtime.InteropServices;
-//using System.Runtime.InteropServices.ComTypes;
-//using System.Security.Cryptography.X509Certificates;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Xml;
+using System;
+
 
 namespace Lab1_Constructors
 {
-
-    public abstract class Instrument
+    public abstract class Instrument : IDisposable
     {
         private string material;
         private bool isTuned;
-        static string owner;
+        static private string owner;
+        private bool disposed;
 
         static Instrument()
         {
             owner = "Maryna Polishchuk";
         }
-        public Instrument() // дефолтний 
+        public Instrument() // дефолтний конструктор
         {
             this.material = "Unknown";
             this.isTuned = false;
             Console.WriteLine("Default Instrument constructor is working");
         }
-        public Instrument(string material, bool isTuned)
+        public Instrument(string material, bool isTuned) // параметризований конструктор
         {
             this.material = material;
             this.isTuned = isTuned;
             Console.WriteLine("Parameters Instrument constructor is working");
         }
-        public virtual void Play()
+        public virtual void Play() // віртуальна функція
         {
             Console.WriteLine("Can be overridden");
         }
@@ -50,7 +43,6 @@ namespace Lab1_Constructors
                 }
                 else
                 {
-
                     this.isTuned = false;
                     Console.WriteLine("instrument need to tuning");
 
@@ -74,7 +66,7 @@ namespace Lab1_Constructors
             }
         }
 
-        private bool disposed;
+        
         public void Dispose()
         {
             Dispose(true);
@@ -83,16 +75,17 @@ namespace Lab1_Constructors
 
         protected virtual void Dispose(bool disposing)
         {
-            if (!this.disposed)
+            if (!this.disposed) // Перевірка, чи вже був об'єкт звільнений
             {
                 if (disposing)
                 {
-                    Console.WriteLine("Disposing managed resourсes");
+                    Console.WriteLine("Disposing managed resourсes"); // Звільнення керованих ресурсів
                 }
-                Console.WriteLine("Disposing unmanaged resourсes");
-                disposed = true;
+                Console.WriteLine("Disposing unmanaged resourсes"); // Звільнення некерованих ресурсів
+                disposed = true; // Позначаємо, що ресурси вже звільнені
             }
         }
+
         ~Instrument()
         {
             Dispose(false);
@@ -102,13 +95,13 @@ namespace Lab1_Constructors
 
 
     }
-    public class PercussionInstrument : Instrument
+    public class PercussionInstrument : Instrument // ударні інстурменти 
     {
-        public PercussionInstrument() // ударні інстурменти 
+        public PercussionInstrument() // дефолтний конструктор
         {
             Console.WriteLine("Default constructor PercussionInstrument is working");
         }
-        public PercussionInstrument(string material, bool isTurned) : base(material, isTurned)
+        public PercussionInstrument(string material, bool isTurned) : base(material, isTurned) // параметризований конструктор
         {
             Console.WriteLine("Parameters constructor PercussionInstrument is working");
         }
@@ -121,22 +114,22 @@ namespace Lab1_Constructors
 
     public class Drum:PercussionInstrument // барабан наслідується від ударних
     {
-        public Drum() 
+        public Drum() // дефолтний конструктор
         {
             Console.WriteLine("Default constructor Drum is working");
         }
-        public Drum(string material, bool isTurned) : base(material, isTurned)
+        public Drum(string material, bool isTurned) : base(material, isTurned) //  параметризований
         {
             Console.WriteLine("Parameters constructor Drum is working");
         }
-        public override void Play()
+        public override void Play()  
         {
             Console.WriteLine("Playing the Drum");
         }
         ~Drum()
         {
             Dispose(false);
-            Console.WriteLine("TrueSeal destructor is used");
+            Console.WriteLine("Drum destructor is used");
         }
     }
     public class SpecificDrum : Drum // специфічний барабан наслідується від барабанів
@@ -145,14 +138,14 @@ namespace Lab1_Constructors
         private static readonly long validId = 561935830;
         private long id;
 
-        private SpecificDrum(long id)
+        private SpecificDrum(long id) // приватний конструктор
         {
             this.id = id;
             Console.WriteLine("SpecificDrum constructor is working");
         }
 
 
-        public static SpecificDrum GetInstance(long id)
+        public static SpecificDrum GetInstance(long id) // публічний статичний конструктор
         {
             if (instance == null && id == validId)
             {
@@ -177,7 +170,7 @@ namespace Lab1_Constructors
         ~SpecificDrum()
         {
             Dispose(false);
-            Console.WriteLine("TrueSeal destructor is used");
+            Console.WriteLine("SpecificDruml destructor is used");
         }
     }
 
@@ -198,7 +191,7 @@ namespace Lab1_Constructors
         ~StringInstrument()
         {
             Dispose(false);
-            Console.WriteLine("TrueSeal destructor is used");
+            Console.WriteLine("StringInstrument destructor is used");
         }
 
     }
@@ -220,29 +213,29 @@ namespace Lab1_Constructors
         ~Piano()
         {
             Dispose(false);
-            Console.WriteLine("TrueSeal destructor is used");
+            Console.WriteLine("Piano destructor is used");
         }
     }
     public class Guitar : StringInstrument
     {
-        //private bool isElectric;
-        //public bool IsElectric
-        //{
-        //    get { return isElectric; }
-        //    set
-        //    {
-        //        isElectric = value;
-        //        Console.WriteLine(isElectric ? "This is an electric guitar." : "This is an acoustic guitar.");
-        //    }
-        //}
+        private bool isElectric;
+        public bool IsElectric
+        {
+            get { return isElectric; }
+            set
+            {
+                isElectric = value;
+                Console.WriteLine(isElectric ? "This is an electric guitar." : "This is an acoustic guitar.");
+            }
+        }
         public Guitar() : base(6) 
         {
             Console.WriteLine("Default constructor Guitar is working");
         }
-        public Guitar(string material, bool isTerned, int numberOfStrings/*, bool isElectric*/) : base(material, isTerned, numberOfStrings)
+        public Guitar(string material, bool isTerned, int numberOfStrings, bool isElectric) : base(material, isTerned, numberOfStrings)
         {
             Console.WriteLine("Parameters constructor Guitar is working");
-            //this.isElectric = isElectric;
+            this.isElectric = isElectric;
 
         }
         public override void Play()
@@ -252,7 +245,27 @@ namespace Lab1_Constructors
         ~Guitar()
         {
             Dispose(false);
-            Console.WriteLine("TrueSeal destructor is used");
+            Console.WriteLine("Guitar destructor is used");
+        }
+    }
+    public class InstrumentsCounter
+    {
+        //Приватний конструктор, щоб не можна було створити об'єкт класу
+        private InstrumentsCounter() { }
+
+        public static int currentCount;
+        static InstrumentsCounter()//Для ініціалізації статичних даних
+        {
+            currentCount = 0;
+        }
+
+        public static int IncrementCount()
+        {
+            return ++currentCount;
+        }
+        public static void Amount()
+        {
+            Console.WriteLine($"There are {currentCount} instruments.");
         }
     }
 
@@ -261,7 +274,27 @@ namespace Lab1_Constructors
         static void Main(string[] args)
         {
             Console.WriteLine("creating new guitar");
-            Guitar guitar = new Guitar("wood", true, 6);
+            //Guitar guitar = new Guitar("wood", true, 6);
+            for (int i = 0; i < 15; i++)
+            {
+                new Piano($"{i}",true, 90);
+            }
+            StringInstrument seal = new StringInstrument(88);
+            InstrumentsCounter.Amount();
+
+            //Виклик GC
+            Console.WriteLine($"Total memory: {GC.GetTotalMemory(false)} bytes");//Загальна кількість використаної пам'яті
+            Console.WriteLine($"Generation of {seal.Material}: {GC.GetGeneration(seal)}.");
+            GC.ReRegisterForFinalize(seal);
+            GC.Collect();//Виклик збирання сміття
+            GC.WaitForPendingFinalizers();//Очікує повне звільнення ресурсів
+            Console.WriteLine($"Total memory after GC: {GC.GetTotalMemory(true)} bytes.");
+            Console.WriteLine($"Generation of {seal.Material}: {GC.GetGeneration(seal)}.");
+
+
+            //Примусовий виклик GC
+
+            Console.ReadKey();
 
         }
     }
